6da765b09ad52fdbb9fe293d302aa2c0ed3111d3
pythoncreate <crstuart@gmail.com>
2017-03-01 21:13:36 -0500

    ship class added

--- ship.py
+++ ship.py
@@ -1,16 +1,18 @@
-class Ship(object):
+class Ship():
 
-    def __init__ (self, name):
-        self.name = ship
+    def __init__ (self, ship_name, size, player):
+        self.ship_name = ship_name
+        self.size =size
+        self.player = player
 
-    def prompt(self,name):
+    def get_ship_location(self):
         while True:
-            aircraft_one = input(" please place your aircraft carrier (5 spaces)--example (A2): ")
-            air_one_strip = aircraft_one.strip()
-            lower_air_one = air_one_strip.lower()
+            ship_location = input(" please place your" + self.ship_name + "("+ self.size + " spaces--example (A2): ")
+            ship_location_strip = ship_location.strip()
+            lower_ship_location = ship_location_strip.lower()
 
-            if len(lower_air_one) in (2,3):
-                if lower_air_one[0] not in 'abcdefghij' or lower_air_one[1:] not in '1,2,3,4,5,6,7,8,9,10':
+            if len(lower_ship_location) in (2,3):
+                if lower_ship_location[0] not in 'abcdefghij' or lower_ship_location[1:] not in '1,2,3,4,5,6,7,8,9,10':
                     print("Oops!  That was not a valid entry.  Try again...")
                     continue
 
