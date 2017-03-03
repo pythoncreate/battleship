a111c64fcf8e2ffab96fe6dcfd588fa9b483af1a
Chris Stuart <ChrisStuart@Commonwealth.com>
2017-03-02 19:10:15 -0500

    added new method to collect ship variables

--- battleship.py
+++ battleship.py
@@ -1,3 +1,5 @@
+from ship import Ship
+
 SHIP_INFO = [
     ("Aircraft Carrier", 5),
     ("Battleship", 4),
@@ -38,157 +40,14 @@ print("\n")
 print_board(board)
 print("\n")
 
-#prompt user one for aircraft carrier coordinates and position
+# define player one's fleet
 
-while True:
-    aircraft_one = input(player1.title() + " please place your aircraft carrier (5 spaces)"
-                                                                 "--example (1E): ")
-    air_one_strip = aircraft_one.strip()
-    lower_air_one = air_one_strip.lower()
-       
-    if lower_air_one[0] not in '1234567890' or lower_air_one[1] not in 'abcdefghij':
-        print("Oops!  That was not a valid entry.  Try again...")
-        continue
-
-    else:
-        break
-
-while True:    
-    aircraft_one_position = input("Is it Horizontal(Y/N?): ")
-    air_one_position_strip = aircraft_one_position.strip()
-    lower_air_one_position = air_one_position_strip.lower()
-
-    if lower_air_one_position not in 'ny':
-        print("Oops!  That was not a valid entry.  Try again...")
-        continue
-
-    else:
-        break
-   
-print("\n")
-clear_screen()
-print_board(board)
-
-#prompt user one for battleship coordinates and position
-
-while True:
-    battleship_one = input(player1.title() + " please place your battleship (4 spaces)"
-                                                                 "--example (1E): ")
-    battleship_one_strip = battleship_one.strip()
-    lower_battleship_one = battleship_one_strip.lower()
-        
-    if lower_battleship_one[0] not in '1,2,3,4,5,6,7,8,9,10' or lower_battleship_one[1] not in 'abcdefghij':
-        print("Oops!  That was not a valid entry.  Try again...")
-        continue
-
-    else:
-        break
-
-while True:    
-    battleship_one_position = input("Is it Horizontal(Y/N?): ")
-    battle_one_position = battleship_one_position.strip()
-    lower_battleship_one_position = battle_one_position.lower()
-
-    if lower_battleship_one_position not in 'ny':
-        print("Oops!  That was not a valid entry.  Try again...")
-        continue
-
-    else:
-        break
+for ship, size in SHIP_INFO:
+    ship = Ship(ship, size, coordinates, direction)
+    place_fleet(player1)
 
 
-print("\n")
-clear_screen()
-print_board(board)
-
-# prompt user one for submarine coordinates and position
-
-while True:
-    submarine_one = input(player1.title() + " please place your submarine (3 spaces)"
-                                                                 "--example (1E): ")
-    sub_one_strip = submarine_one.strip()
-    lower_submarine_one = sub_one_strip.lower()
-        
-    if lower_submarine_one[0] not in '1,2,3,4,5,6,7,8,9,10' or lower_submarine_one[1] not in 'abcdefghij':
-        print("Oops!  That was not a valid entry.  Try again...")
-        continue
-
-    else:
-        break
-
-while True:    
-    submarine_one_position = input("Is it Horizontal(Y/N?): ")
-    sub_one_position = submarine_one_position.strip()
-    lower_submarine_one_position = sub_one_position.lower()
-
-    if lower_submarine_one_position not in 'ny':
-        print("Oops!  That was not a valid entry.  Try again...")
-        continue
-
-    else:
-        break
 
 
-print("\n")
-clear_screen()
-print_board(board)
-
-# prompt user one for cruiser coordinates and position
-
-while True:
-    cruiser_one = input(player1.title() + " please place your cruiser (3 spaces)"
-                                                                 "--example (1E): ")
-    cruiser_one_strip = cruiser_one.strip()
-    lower_cruiser_one = cruiser_one_strip.lower()
-        
-    if lower_cruiser_one[0] not in '1,2,3,4,5,6,7,8,9,10' or lower_cruiser_one[1] not in 'abcdefghij':
-        print("Oops!  That was not a valid entry.  Try again...")
-        continue
-
-    else:
-        break
-
-while True:    
-    cruiser_one_position = input("Is it Horizontal(Y/N?): ")
-    cruise_one_position = cruiser_one_position.strip()
-    lower_cruiser_one_position = cruise_one_position.lower()
-
-    if lower_cruiser_one_position not in 'ny':
-        print("Oops!  That was not a valid entry.  Try again...")
-        continue
-
-    else:
-        break
-
-print("\n")
-clear_screen()
-print_board(board)
-
-# prompt user one for patrol boat coordinates and position
-
-while True:
-    patrol_one = input(player1.title() + " please place your patrol (2 spaces)"
-                                                                 "--example (1E): ")
-    patrol_one_strip = patrol_one.strip()
-    lower_patrol_one = patrol_one_strip.lower()
-        
-    if lower_patrol_one[0] not in '1,2,3,4,5,6,7,8,9,10' or lower_patrol_one[1] not in 'abcdefghij':
-        print("Oops!  That was not a valid entry.  Try again...")
-        continue
-
-    else:
-        break
-
-while True:    
-    patrol_one_position = input("Is it Horizontal(Y/N?): ")
-    pat_one_position = patrol_one_position.strip()
-    lower_patrol_one_position = pat_one_position.lower()
-
-    if lower_patrol_one_position not in 'ny':
-        print("Oops!  That was not a valid entry.  Try again...")
-        continue
-
-    else:
-        break
 
 
--- ship.py
+++ ship.py
@@ -1,20 +1,21 @@
 class Ship():
 
-    def __init__(self, ship_name, size, player):
+    def __init__(self, ship_name, size, coordinates, direction):
         self.ship_name = ship_name
         self.size = size
-        self.player = player
+        self.coordinates = coordinates
+        self.direction = direction
 
-    def get_ship_location(self):
+    def get_ship_coordinates(self):
         while True:
-            ship_location = input('Please place your ' + self.ship_name + '('
+            ship_coords = input('Please place your ' + self.ship_name + '('
                             + str(self.size) + ' spaces--example (A2)): ')
-            ship_location_strip = str(ship_location).strip()
-            lower_ship_location = str(ship_location_strip.lower())
+            ship_coords_strip = str(ship_coords).strip()
+            coordinates = str(ship_coords_strip.lower())
 
-            if len(lower_ship_location) in range(2,4):
-                if lower_ship_location[0] not in 'abcdefghij' or \
-                   lower_ship_location[1:] not in '1,2,3,4,5,6,7,8,9,10':
+            if len(coordinates) in range(2,4):
+                if coordinates[0] not in 'abcdefghij' or \
+                   coordinates[1:] not in '1,2,3,4,5,6,7,8,9,10':
                     print("Oops!  That was not a valid entry.  Try again...")
                     continue
 
@@ -23,10 +24,23 @@ class Ship():
                     break
 
             else:
-                if len(lower_ship_location) < 2 or len(lower_ship_location) > 3:
+                if len(coordinates) < 2 or len(lower_ship_location) > 3:
                     print("Oops!  That's too not the right amount of characters. Please try again...")
                     continue
 
+    def get_ship_direction(self):
+        while True:
+            ship_direction = input("Is it Horizontal(Y/N?): ")
+            ship_direction_strip = ship_direction.strip()
+            lower_ship_direction = ship_direction_strip.lower()
 
+            if lower_ship_direction not in 'ny':
+                print("Oops!  That was not a valid entry.  Try again...")
+                continue
 
+            else:
+                break
 
+    def place_fleet(self, player):
+        coord = get_ship_coordinates()
+        position = get_ship_direction()
